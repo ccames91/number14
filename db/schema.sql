738c5ebc2d9b8a35613d88365b6b1c2f163a1320
chandler ames <eddiehawk1991@gmail.com>
2023-10-20 14:11:19 -0700

    restart but keep package.json

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS tech_table_db;
+CREATE DATABASE tech_table_db;
--- public/script.js
+++ public/script.js
@@ -1,23 +1,3 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    const loginButton = document.getElementById('loginButton');
-    if (loginButton) {
-      loginButton.addEventListener('click', () => {
-        console.log('Login button clicked');
-      });
-    }
-  });
-  
-  function fetchData() {
-    fetch('/api/some-data')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Data retrieved:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }
-  
  
   
