d5e746487812621d4304c78d1fad90084a5aff99
chandler ames <eddiehawk1991@gmail.com>
2023-10-20 00:55:48 -0700

    make it better

---
+++ routes/api-routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+router.get('/api/data', async (req, res) => {
+  try {
+    const data = await db.YourModel.findAll();
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.post('/api/data', async (req, res) => {
+  try {
+    const { newData } = req.body;
+
+    const createdData = await db.YourModel.create({ property: newData });
+
+    res.json(createdData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+module.exports = router;
