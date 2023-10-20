f4d4ce2c0bd553af88f2d25cb442e96c80c902c0
chandler ames <eddiehawk1991@gmail.com>
2023-10-20 14:25:14 -0700

    file structer and config

--- models/index.js
+++ models/index.js
@@ -1,2 +1,20 @@
+const Sequelize = require('sequelize');
+const db = {};
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+);
+
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
 
 
--- server.js
+++ server.js
@@ -1,1 +1,30 @@
+const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const dotenv = require('dotenv');
+const db = require('./models'); 
+const app = express();
+
+
+dotenv.config();
+
+
+const sessionStore = new SequelizeStore({
+  db: db.sequelize,
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
